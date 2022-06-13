8883ea0e470f6380765b9e175defc4e1bc9cb9ec
chl5566 <tukuchl5566@gmail.com>
2022-06-13 17:55:31 +0800

    add auth

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { authRouter, userRouter } = require("./routers");
+const { authRouter, userRouter, lessonRouter } = require("./routers");
 const pool = require("./utils/dbConnect");
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 啟用 session
+const expressSession = require("express-session");
+app.use(
+  expressSession({ secret: "test", resave: false, saveUninitialized: false })
+);
 
 app.get("/", (req, res) => {
   res.send("sweteen server");
@@ -14,6 +28,7 @@ app.get("/", (req, res) => {
 // NOTE routers
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/lesson", lessonRouter);
 
 // NOTE 404
 app.use((req, res, next) => {
