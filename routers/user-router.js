7f447364c97b3818fc5ea04a7412298b243f77b9
= <=>
2022-06-21 16:01:54 +0800

    頁碼新增and撈取資料

--- routers/user-router.js
+++ routers/user-router.js
@@ -12,37 +12,28 @@ router.use((req, res, next) => {
   console.log("request is comming userRouter");
   next();
 });
+
 // 資料驗證要使用的東西
+// 評論建立驗證
 const commentScoretRules = [
   body("content").isLength({ min: 1 }).withMessage("請至少評論1個字以上"),
   body("score").isLength({ min: 1 }).withMessage("請給分數"),
 ];
+// 內容更改驗證
 const contentChangeRules = [
   body("content")
     .isLength({ min: 1 })
     .withMessage("請至少評論一個字以上(單純改評論)"),
 ];
+// 分數更改驗證
 const scoreChangeRules = [
   body("score")
     .isLength({ min: 1 })
     .withMessage("請請至少給出一點分數吧(單純只改分數)"),
 ];
 
-// TODO 會員評論 READ
-router.get("/comment/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    let [commentData] = await pool.execute("SELECT * FROM comment WHERE id=?", [
-      id,
-    ]);
-    res.send(commentData);
-  } catch (e) {
-    res.send(e);
-  }
-});
-
 // TODO 會員評論 CREATE
-// TODO 評論(字數)&(分數)的驗證-----------(v)
+// TODO 評論(字數)&(分數)的驗證
 router.post("/comment", commentScoretRules, async (req, res, next) => {
   let created_at = new Date();
   let { id, user_id, product_id, content, score } = req.body;
@@ -69,7 +60,7 @@ router.delete("/comment/:id", async (req, res, next) => {
 });
 
 // TODO 評論 UPDATE
-// TODO 單改評論(字數)更新驗證----------------(v)
+// TODO 單改評論(字數)更新驗證
 router.patch("/comment/:id", contentChangeRules, async (req, res, next) => {
   let { id } = req.params;
   let { content } = req.body;
@@ -124,15 +115,34 @@ router.get("/score/:id", async (req, res, next) => {
   }
 });
 
-// TODO 喜好 READ
-router.get("/favorite_product/:id", async (req, res, next) => {
-  let { id } = req.params;
-  let { product_id } = req.body;
-  let [readDataFavoriteProduct] = await pool.execute(
-    "SELECT * FROM favorit_product WHERE user_id=?",
-    [id]
+// TODO 喜好 READ & 喜好數量的頁碼
+router.get("/favorite_product/:user_id", async (req, res, next) => {
+  // 1. 取得目前在第幾頁 ， 且利用 || 這個特性來做預設值
+  let page = req.query.page || 1;
+  // 2. 取得目前的總比數
+  let [allResults] = await pool.execute(
+    "SELECT * FROM favorit_product WHERE user_id = ?",
+    [req.params.user_id]
   );
-  res.send(readDataFavoriteProduct);
+  const total = allResults.length;
+  // 3. 計算總空有幾頁
+  const perPage = 2;
+  const totalPage = Math.ceil(total / perPage);
+  // 4. 計算offect 是多少(計算要跳過幾筆)
+  let offset = (page - 1) * perPage;
+  // 5. 取得這一頁的資料
+  let [readDataFavoriteProduct] = await pool.execute(
+    "SELECT * FROM favorit_product, product WHERE favorit_product.product_id=product.id AND user_id=? LIMIT ? OFFSET ?",
+    [req.params.user_id, perPage, offset]
+  );
+  res.json({
+    pagination: {
+      total,
+      totalPage,
+      page,
+    },
+    allResults: readDataFavoriteProduct,
+  });
 });
 
 // TODO 喜好 DELETE
@@ -154,6 +164,41 @@ router.post("/favorite_product", async (req, res, next) => {
     [user_id, product_id]
   );
   res.send(`會員 ${user_id} 您的喜愛商品 ${product_id} 已經更新囉`);
+});
+
+// TODO 建立會員評論總數的頁碼 & 讀取資料
+router.get("/comment/:user_id", async (req, res, next) => {
+  // 1. 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  let page = req.query.page || 1;
+  console.log("current page", page);
+  // 2. 取得目前總比數
+  let [allResults, fields] = await pool.execute(
+    "SELECT * FROM comment WHERE user_id = ?",
+    [req.params.user_id]
+  );
+  // res.json(allResults);
+  // console.log(allResults);
+  const total = allResults.length;
+  // 3. 計算總共有幾頁
+  const perPage = 2;
+  const lastPage = Math.ceil(total / perPage);
+  // 4. 計算 offect 是多少(計算要跳過幾筆)
+  let offect = (page - 1) * perPage;
+  // 5. 取得一頁的資料
+  let [pageResult] = await pool.execute(
+    "SELECT product.name as product_name, product.price, user.full_name, comment.content, comment.score FROM comment, product, user WHERE user.id=comment.user_id AND comment.product_id=product.id AND user_id = ?  LIMIT ? OFFSET ?",
+    [req.params.user_id, perPage, offect]
+  );
+  // 6. 回覆給前端的資料
+  res.json({
+    // 用來儲存所有跟頁碼有關的資訊
+    pagination: {
+      total,
+      lastPage,
+      page,
+    },
+    allResults: pageResult,
+  });
 });
 
 module.exports = router;
