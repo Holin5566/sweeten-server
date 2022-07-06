0b2c99743cc5401678af5cf48d1e97e02d61a339
“yhanh” <yhanh999@gmail.com>
2022-07-06 10:49:09 +0800

    adjust product and expriy routers

--- routers/expriy-router.js
+++ routers/expriy-router.js
@@ -48,10 +48,10 @@ router.get("/expire_product", async (req, res, next) => {
 
     // 取得這一頁的資料 select * ... limit ? offset ?
     let [pageResult] = await pool.execute(
-      `SELECT name, price,expiry.id,count,expiry.expiry_date FROM product, expiry WHERE expiry.product_id=product.id LIMIT ? OFFSET ?`,
+      `SELECT name, price,count,expiry.* FROM product, expiry WHERE expiry.product_id=product.id LIMIT ? OFFSET ?`,
       [perPage, offset]
     );
-    console.log(pageResult);
+    // console.log(pageResult);
 
     // 回覆給前端
     if (pageResult.length === 0) {
